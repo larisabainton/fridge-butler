a5ea562e6836c40a21fe76e996891fb4a4384b7e
Larisa <larisabainton@gmail.com>
2016-10-16 13:58:12 -0400

    added devise

---
+++ spec/features/user_log_in_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'log in' do
+  let!(:jon) { FactoryGirl.create(:user) }
+  context 'as a user' do
+    scenario 'user provides valid user information for log-in' do
+      user_sign_in(jon)
+
+      expect(page).to have_content('Signed in successfully.')
+    end
+
+    scenario 'user can log off' do
+      user_sign_in(jon)
+      visit root_path
+      click_link 'Log Off'
+
+      expect(page).to have_content 'Signed out successfully'
+    end
+  end
+end
