80f4b70ea08b9c82e5cb348eb90e008cdf65cdd8
Larisa <larisabainton@gmail.com>
2016-10-16 17:01:48 -0400

    user sign in tests created

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -1,29 +1,73 @@
 require 'rails_helper'
 
 feature 'sign up' do
+  let!(:user) { FactoryGirl.create(:user, email: 'example@example.com') }
+
   context 'As a user' do
     scenario 'I see a signup button on the home page' do
+      visit root_path
+
+      expect(page).to have_content('Sign Up')
     end
 
     scenario 'I can click on the sign up button' do
+      visit root_path
+      click_link 'Sign Up'
+
+      expect(page).to have_content('Password')
+      expect(page).to have_content('Email')
     end
 
     scenario 'I can enter my information and sign up' do
+      visit root_path
+      click_link 'Sign Up'
+      fill_in 'First Name', with: 'John'
+      fill_in 'Last Name', with: 'Smith'
+      fill_in 'Email', with: 'example2@example.com'
+      fill_in 'Password', with: 'password'
+      fill_in 'Password Confirmation', with: 'password'
+      click_button 'Sign up'
+
+      expect(page).to have_content('Welcome! You have signed up successfully.')
     end
 
     scenario 'I can\'t enter an email already in the system' do
+      visit root_path
+      click_link 'Sign Up'
+      fill_in 'First Name', with: 'John'
+      fill_in 'Last Name', with: 'Smith'
+      fill_in 'Email', with: 'example@example.com'
+      fill_in 'Password', with: 'password'
+      fill_in 'Password Confirmation', with: 'password'
+      click_button 'Sign up'
+
+      expect(page).to have_content('Email has already been taken')
     end
 
     scenario 'I can\'t enter a password confirmation that doesn\'t match' do
+      visit root_path
+      click_link 'Sign Up'
+      fill_in 'First Name', with: 'John'
+      fill_in 'Last Name', with: 'Smith'
+      fill_in 'Email', with: 'example3@example.com'
+      fill_in 'Password', with: 'password'
+      fill_in 'Password Confirmation', with: 'passward'
+      click_button 'Sign up'
+
+      expect(page).to have_content('Password confirmation doesn\'t match')
     end
 
     scenario 'I can\'t enter a password that is too short' do
-    end
+      visit root_path
+      click_link 'Sign Up'
+      fill_in 'First Name', with: 'John'
+      fill_in 'Last Name', with: 'Smith'
+      fill_in 'Email', with: 'example4@example.com'
+      fill_in 'Password', with: 'pass'
+      fill_in 'Password Confirmation', with: 'pass'
+      click_button 'Sign up'
 
-    scenario 'I can log out' do
-    end
-
-    scenario 'I can log in if I already have an account' do
+      expect(page).to have_content('Password is too short')
     end
   end
 end
