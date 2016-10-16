0ec7b949b3b5d423b3e4d9203f6be73865e84a46
Larisa <larisabainton@gmail.com>
2016-10-16 17:17:22 -0400

    added test for log in

--- spec/features/user_log_in_spec.rb
+++ spec/features/user_log_in_spec.rb
@@ -1,11 +1,28 @@
 require 'rails_helper'
 
 feature 'log in' do
+  let!(:user) { FactoryGirl.create(:user) }
   context 'As a user' do
     scenario 'I can log in if I already have an account' do
+      visit root_path
+      click_link 'Log In'
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Log in'
+
+      expect(page).to have_content('Signed in successfully')
     end
 
     scenario 'I can log out' do
+      visit root_path
+      click_link 'Log In'
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Log in'
+      click_link 'Log Off'
+
+      expect(page).to have_content('lblaieawle')
+
     end
   end
 end
