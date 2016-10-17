7a0c4c482b153256e67ec87da71151ef7f4e472d
Larisa <larisabainton@gmail.com>
2016-10-17 11:56:27 -0400

    tests for new fridge

--- app/controllers/fridges_controller.rb
+++ app/controllers/fridges_controller.rb
@@ -1,6 +1,11 @@
 class FridgesController < ApplicationController
-
   def index
+    if user_signed_in?
+      @user = current_user
+      @fridge = user.fridge
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :fridges
+  has_one :fridge
 
   validates :first_name, presence: true
   validates :last_name, presence: true
