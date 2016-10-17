5b91ef8854438fbc07de0705bd96e83c8e0a9bdd
Larisa <larisabainton@gmail.com>
2016-10-17 16:32:57 -0400

    WIP tests

---
+++ spec/factories/foodcategories.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :foodcategory do
+    name 'Dairy'
+    fridge_id 1
+  end
+end
--- spec/features/user_views_fridge_spec.rb
+++ spec/features/user_views_fridge_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 feature 'view fridge' do
   let!(:user) { FactoryGirl.create(:user) }
   let!(:fridge) { FactoryGirl.create(:fridge, user_id: user.id ) }
+  let!(:dairy) { FactoryGirl.create(:foodcategory, user_id: user.id) }
+  let!(:vegetables) { FactoryGirl.create(:foodcategory, name: 'Vegetables', user_id: user.id) }
+  let!(:meat) { FactoryGirl.create(:foodcategory) }
   context 'As a user' do
     before(:each) do
       visit root_path
@@ -13,9 +16,16 @@ feature 'view fridge' do
     end
 
     scenario 'I can see the name of my fridge on the show page' do
+      expect(page).to have_content(fridge.name)
     end
 
     scenario 'I can see the food categories on the show page of my fridge' do
+      expect(page).to have_content(dairy.name)
+      expect(page).to have_content(vegetables.name)
+    end
+
+    scenario 'I don\'t see food categories on the show page of my fridge that I didn\'t create' do
+      expect(page).not_to have_content(meat.name)
     end
 
     scenario 'I can add new food categories on the show page of my fridge' do
