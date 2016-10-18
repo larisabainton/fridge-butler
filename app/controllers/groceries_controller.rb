12c55f7d05fbfe72f8b32b849fb61e162b409cc8
Larisa <larisabainton@gmail.com>
2016-10-18 15:10:36 -0400

    WIP collection_radio_button not saving as a number, exp_date should probably be a date instead of a string

---
+++ app/controllers/groceries_controller.rb
@@ -0,0 +1,36 @@
+class GroceriesController < ApplicationController
+  def new
+    if user_signed_in?
+      @grocery = Grocery.new
+    else
+      flash[:notice] = "Please sign in"
+      redirect_to new_user_session_path
+    end
+  end
+
+  def create
+    @grocery = Grocery.new(grocery_params)
+    @user = current_user
+    @fridge = @user.fridge
+
+    if @grocery.save
+      flash[:notice] = "Category added successfully"
+      redirect_to fridge_path(@fridge)
+    else
+      flash[:notice] = @grocery.errors.full_messages.join(', ')
+      render 'new'
+    end
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+
+  def grocery_params
+    params.require(:grocery).permit(:name, :quantity, :exp_date, :fridgecategory)
+  end
+end
